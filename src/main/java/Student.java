297953b5f33450dcee6ae5f61f7c03454f5eb0f1
Amida Fombutu <amida.fombutu@gmail.com>
2022-10-24 14:05:14 -0500

    ex

---
+++ src/main/java/Student.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer>grades;
+
+    public Student(int id, String name,ArrayList<Integer>grades){
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+
+    // returns the student's id
+    public long getId(){return this.id;}
+
+    // returns the student's name
+    public String getName(){return this.name;}
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+        this.grades.add(grade);
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return this.grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        float total = 0;
+        for (int grade:grades){
+            total += grade;
+        }
+        return total/grades.size();
+    }
+
+
+}
